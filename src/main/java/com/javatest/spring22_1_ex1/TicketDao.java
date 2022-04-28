9480e833f49a39bfba47d19485f9fe4174643227
ici806 <evergreen0208@hanmail.net>
2022-04-28 09:01:38 +0900

    transaction

--- src/main/java/com/javatest/spring22_1_ex1/TicketDao.java
+++ src/main/java/com/javatest/spring22_1_ex1/TicketDao.java
@@ -8,7 +8,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.AbstractPlatformTransactionManager;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 
@@ -18,24 +17,29 @@ public class TicketDao {
 	TransactionTemplate transactionTemplate;
 	
 	PlatformTransactionManager transactionManager;
+	
+	
+	
+	public void setTransactionManager(PlatformTransactionManager transactionManager) {
+		this.transactionManager = transactionManager;
+	}
 
 	public void setTemplate(JdbcTemplate template) {
 		this.template = template;
 	}
+
 	public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
 		this.transactionTemplate = transactionTemplate;
 	}
-	public void setTransactionManager(PlatformTransactionManager transactionManager) {
-		this.transactionManager = transactionManager;
-	}
-	
+
+
 	public TicketDao() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
 	
 	public void buyTicket(final TicketDto dto) {
 		
-		// 카드 결제와 티켓 구매를 묶어주기
 		
 		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
 			
@@ -43,56 +47,56 @@ public class TicketDao {
 			protected void doInTransactionWithoutResult(TransactionStatus status) {
 				// TODO Auto-generated method stub
 				
+				
 				try {
 				template.update(new PreparedStatementCreator() {
 					
-					// 카드 결제
+					//카드 결제
 					@Override
 					public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
 						// TODO Auto-generated method stub
-						String query="insert into card (consumerid, amount) values (?,?)";
 						
+						String query="INSERT INTO card (consumerid, amount) VALUES (?, ?)";
 						PreparedStatement pstmt = con.prepareStatement(query);
-						pstmt.setString(1,dto.getConsumerid());
-						pstmt.setInt(2,dto.getAmount());
+						pstmt.setString(1, dto.getConsumerid());
+						pstmt.setInt(2, dto.getAmount());
 						
 						return pstmt;
 					}
 				});
 				
-				// 티켓 구매(창구)
 				template.update(new PreparedStatementCreator() {
 					
+					//티켓 구매
 					@Override
 					public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-						String query="insert into ticket (consumerid, countNum) values (?,?)";
-						
+						// TODO Auto-generated method stub
+						String query="INSERT INTO ticket (consumerid, countnum) VALUES (?, ?)";
 						PreparedStatement pstmt = con.prepareStatement(query);
-						pstmt.setString(1,dto.getConsumerid());
-						pstmt.setInt(2,dto.getAmount());
+						pstmt.setString(1, dto.getConsumerid());
+						pstmt.setInt(2, dto.getAmount());
 						
 						return pstmt;
 					}
 				});
 				
-				transactionManager.commit(status);
+					
 				
-				
-			} catch(Exception e) {
-				e.printStackTrace();
-				
-				HomeController cont = new HomeController();
-				
-				cont.countError();
-				
-				System.out.println("Rollback!");
-			}
-				
+				}catch(Exception e) {
+					e.printStackTrace();
+					
+					HomeController cont = new HomeController();
+					
+					cont.countError();
+					
+					System.out.println("Rollback!!!!!");
+				}
 			}
 		});
 		
 		
+		
+		
 	}
-
 	
 }
